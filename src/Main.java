e9136a0164952e450095d9eb6250fdac55849ff5
alexandre <alexandrekdias11@gmail.com>
2023-02-12 12:49:05 +0000

    echo "# jetBrains_course" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/jabroJr94/jetBrains_course.git git push -u origin main

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {  // declaração da classe Main e inicio do mesmo
+       public static void main(String[] args) { // inicio do programa
+
+        System.out.println("Hello world!"); //função padrão para exibir mensagem no prompt
+    } //fim do programa
+} //fim da classe
